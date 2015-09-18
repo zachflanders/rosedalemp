084897a9cfcb3c66804363f3dae3b9807376330f
Zach Flanders <zachflanders@gmail.com>
2015-09-18 13:49:36 -0500

    stuff

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,7 +7,7 @@ angular.module('codeApp')
       $scope.addEmail = function(){
           console.log($scope.email);
           var url = "https://api.e2ma.net/1406836/members/add";
-          $http.defaults.headers.common.Authorization = "Basic 9a96842ee8b8c7a568c4";
+          $http.defaults.headers.common.Authorization = "Basic ";
           $http.defaults.headers.post = {'Access-Control-Allow-Origin': 'http://localhost:9000'};
           $http.post(url,{'email':$scope.email, "group_ids":[2069364]});
       }
