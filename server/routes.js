9bda583f6fb4a753fda69c4db9e3d3bd2ff22583
Zach Flanders <zachflanders@gmail.com>
2015-09-18 13:43:04 -0500

    did some stuff

--- server/routes.js
+++ server/routes.js
@@ -9,9 +9,15 @@ var path = require('path');
 
 module.exports = function(app) {
 
+  app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   }); 
+
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
